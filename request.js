15e68146189ee2b2f85ff54fde1a6707421d8108
chaimae <chaimae.elinawi@gmail.com>
2023-06-18 21:30:42 +0200

    search

--- request.js
+++ request.js
@@ -24,17 +24,11 @@ function get(){
       else{
         alert("error")
       }
-
-
-
-   
+ 
     }
-    
-
 }
 
 get()
-
 
 //create
 function create(){
@@ -57,7 +51,85 @@ function create(){
         //j'affiche un element de responce "body"
         document.getElementById("newObject").innerHTML =`${responce.body}`
         console.log("status code is" + request.status)
+        alert("the post has been created successfully")
         }
 }
 
+
+//update 
+function update(){
+    let request = new XMLHttpRequest()
+    request.open("PUT","https://jsonplaceholder.typicode.com/posts/1")
+    request.responseType = "json"
+    request.setRequestHeader("Accept" ,"application/json")
+    request.setRequestHeader("Content-type" , "application/json")
+       //modification de l'objet
+    let bodyPara =`{
+           "title":"hello word" ,
+           "body":"new film",
+           "userId" :1
+    }`
+    request.send(bodyPara)
+
+    request.onload= function(){
+        let responce = request.response
+        console.log(responce)
+        document.getElementById("newObject").innerHTML =`${responce.body}`
+        console.log("status code is" + request.status)
+        alert("the post has been updated successfully")
+        }
+}
+
+
+
+
+//delete
+function deleteData(){
+    let request = new XMLHttpRequest()
+    request.open("PUT","https://jsonplaceholder.typicode.com/posts/1")
+    request.responseType = "json"
+    request.setRequestHeader("Accept" ,"application/json")
+    request.setRequestHeader("Content-type" , "application/json")
+    request.send()
+
+    request.onload= function(){
+        let responce = request.response
+        console.log(responce)
+        console.log("status code is" + request.status)
+        alert("the post has been deleted successfully")
+        }
+}
+
+
+ ///get by userId 
+      //feltring, get by userId ,get all userId=1, url = url?userId=1,search
+      function search(){
+
+        let request = new XMLHttpRequest()
+        request.open("GET","https://jsonplaceholder.typicode.com/posts?userId=2")
+        request.responseType = "json"
+        request.send()
+        request.onload= function(){
+        let posts = request.response
+        console.log(posts )
+          if(request.status >=200 && request.status <300){
+                for(post of posts){
+                console.log(post.title )
+               
+                }
+                alert("search success")
+          }
+          else{
+            alert("error")
+          }
+     
+        }
+    }
+    
+
+
+
 create()
+update()
+deleteData()
+search()
